814b4d95470cc7f3f8373a8df941710b4e15d7f3
isantidev <santis.sierra6@gmail.com>
2025-12-06 00:38:47 -0500

    Add menu layout and content components for improved UI

---
+++ app/components/ui/MenuContent.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import SandwichCard from "./SandwichCard";
+import ExtraCard from "./ExtraCard";
+import { getMenuData } from "@/lib/data/menu";
+import { Menu } from "@/lib/types/menu.type";
+
+export default function MenuContent() {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [menu, setMenu] = useState<Menu>({ extras: [], sandwiches: [] });
+
+    useEffect(() => {
+        const fetchMenu = async () => {
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+
+            const data = await getMenuData().then((data) => data[0]);
+            setMenu(data);
+            setIsLoading(false);
+        };
+
+        fetchMenu();
+    }, []);
+
+    if (isLoading) {
+        return <p>Loading menu...</p>;
+    }
+
+    return (
+        <>
+            <aside className="w-full row-start-1 @lg:col-start-2">
+                {/* Extras Card Content */}
+                {menu.extras.map((extra) => {
+                    return <ExtraCard key={`extra-${extra.id}`} {...extra} />;
+                })}
+            </aside>
+            <section className="w-full row-start-2 @lg:row-start-1 @lg:col-start-1 grid grid-cols-9 gap-4">
+                {/* Sandwiches Card Content */}
+                {menu.sandwiches.map((sandwich) => {
+                    return (
+                        <SandwichCard
+                            key={`sandwich-${sandwich.id}`}
+                            {...sandwich}
+                        />
+                    );
+                })}
+            </section>
+        </>
+    );
+}
